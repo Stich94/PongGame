a630681f4ec3f48102d333b96ef204c7b8bd19aa
Stich <com>
2021-09-15 14:30:57 +0200

    add ball and move it

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 # screen settings
 screen = Screen()
@@ -10,6 +12,7 @@ screen.tracer(0) # turn off the animation for moving from the center to the new 
 
 l_paddle = Paddle((-350, 0))
 r_paddle = Paddle((350, 0))
+ball = Ball()
 # first is the listen required on the screen after that is the onkey
 screen.listen()
 screen.onkey(r_paddle.up, "Up")
@@ -17,10 +20,15 @@ screen.onkey(r_paddle.down, "Down")
 screen.onkey(l_paddle.up, "w")
 screen.onkey(l_paddle.down, "s")
 
+
+
+
 # required to call a update every single time when using .tracer()
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 
