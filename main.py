8b763bfa5723a2e0d493c356b29a000aef743a22
Stich <com>
2021-09-15 12:31:42 +0200

    change paddle class - pos with tuples

--- main.py
+++ main.py
@@ -8,17 +8,20 @@ screen.bgcolor("black")
 screen.title("Pong Game")
 screen.tracer(0) # turn off the animation for moving from the center to the new pos
 
-paddle = Paddle()
+l_paddle = Paddle((-350, 0))
+r_paddle = Paddle((350, 0))
 # first is the listen required on the screen after that is the onkey
 screen.listen()
-screen.onkey(paddle.up, "Up")
-screen.onkey(paddle.down, "Down")
+screen.onkey(r_paddle.up, "Up")
+screen.onkey(r_paddle.down, "Down")
+screen.onkey(l_paddle.up, "w")
+screen.onkey(l_paddle.down, "s")
 
 # required to call a update every single time when using .tracer()
 game_is_on = True
 while game_is_on:
     screen.update()
 
-# Write the Paddle Class and Create the Second Paddle
+
 
 screen.exitonclick()
