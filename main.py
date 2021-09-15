85eb8da7e58b4d5b421b761292bced9ba7ab594f
Stich <com>
2021-09-15 08:37:20 +0200

    setup screen

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+
+# screen settings
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+
+
+screen.exitonclick()
